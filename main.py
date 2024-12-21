6c93ee0eab47fef09995e7522497b36996535b20
Inesso4ka103 <innaalpar@gmail.com>
2024-12-21 18:41:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,106 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+from crud_functions import *
+
+api = '7599924313:AAH-vWWTT0ILIqMU1ABPqRQDeCw5OnGUuz0'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+button3 = KeyboardButton(text='Купить')
+kb.row(button, button2)
+kb.add(button3)
+
+kb1 = InlineKeyboardMarkup()
+in_button = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+in_button2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+kb1.row(in_button, in_button2)
+
+kb2 = InlineKeyboardMarkup()
+product1 = InlineKeyboardButton(text='Продукт1', callback_data="product_buying")
+product2 = InlineKeyboardButton(text='Продукт2', callback_data="product_buying")
+product3 = InlineKeyboardButton(text='Продукт3', callback_data="product_buying")
+product4 = InlineKeyboardButton(text='Продукт4', callback_data="product_buying")
+kb2.row(product1, product2, product3, product4)
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+start_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Рассчитать норму калорий')],
+        [KeyboardButton(text='Формулы расчёта')]
+    ], resize_keyboard=True
+)
+
+@dp.message_handler(text=['/start'])
+async def start(message):
+    await message.answer(f'Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+@dp.message_handler(text='Информация')
+async def inform(message):
+    await message.answer('Информация о боте.')
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer('Выберите опцию:', reply_markup=kb1)
+
+@dp.message_handler(text='Купить')
+async def get_buying_list(message):
+    for i in range(1, 5):
+        with open(f"images/{i}.png", "rb") as img:
+            await message.answer_photo(img)
+        await message.answer("Выберите продукт для покупки: ", reply_markup=kb2)
+
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.message.answer("Вы успешно приобрели продукт!")
+    await call.answer()
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer("10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) - 161")
+    await call.answer()
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст:')
+    await call.answer()
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    norma = (10*int(data['weight'])+6.25*int(data['growth'])-5*int(data['age'])-161)
+    await message.answer(f'Ваша норма каллорий: {norma}')
+    await UserState.weight.set()
+    await state.finish()
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer(f'Введите команду /start, чтобы начать общение.')
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
